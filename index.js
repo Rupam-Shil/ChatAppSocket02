37d0b2f257f64f052781d6cc089510c521fedd9d
Rupam Shil <rupamshil111@gmail.com>
2021-08-11 22:53:14 +0530

    add new user whenever they join

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
+const generateName = require('sillyname');
 const PORT = process.env.PORT || 9000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('css'));
+
+const io = new Server(server);
+const users = [];
+io.on('connection', (socket) => {
+	const sillyName = generateName();
+	io.emit('new user', sillyName);
+	// users.push({ name: sillyName });
+	// console.log(users);
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
+	socket.on('send Message', (msg) => {
+		socket.broadcast.emit('message', msg);
+	});
+});
+
 app.get('/', (req, res) => {
 	res.render('chat');
 });
